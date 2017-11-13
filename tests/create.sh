d2207f02e7fb531399823b30db0dd1279d94b540
Carlos Santana <csantana@us.ibm.com>
2017-11-12 22:29:38 -0500

    do create wit nodejs:8

--- tests/create.sh
+++ tests/create.sh
@@ -10,7 +10,7 @@ action=$3
 
 # create a noop action
 echo "Creating action $action"
-curl -u "$credentials" "$host/api/v1/namespaces/_/actions/$action" -XPUT -d '{"namespace":"_","name":"test","exec":{"kind":"nodejs:default","code":"function main(){return {};}"}}' -H "Content-Type: application/json"
+curl -u "$credentials" "$host/api/v1/namespaces/_/actions/$action" -XPUT -d '{"namespace":"_","name":"test","exec":{"kind":"nodejs:8","code":"function main(){return {};}"}}' -H "Content-Type: application/json"
 
 # run the noop action
 echo "Running $action once to assert an intact system"
